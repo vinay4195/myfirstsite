e058899b2578842fd4e4c9f3f5dfa4a23984be55
Vinay Kumar <vinay.vk.kumar4@gmail.com>
2022-07-15 20:41:57 +0530

    first commit

---
+++ Home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    context = {'name':'vinay','course':'python'}
+    return render (request, 'home.html',context)
+def about(request):
+    # return HttpResponse ("this is my about page")
+    return render (request, 'about.html')
+def project(request):
+    # return HttpResponse ("this is my project page")
+    return render (request, 'project.html')
+def contact(request):
+    # return HttpResponse ("this is my contact page")
+    return render (request, 'contact.html')
