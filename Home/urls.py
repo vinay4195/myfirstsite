d59be7323360042cdb82f617f0341de7128d901b
vinay4195 <vinay.vk.kumar4@gmail.com>
2022-07-17 20:05:39 +0530

    sixth commit

--- Home/urls.py
+++ Home/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from Home import views
 urlpatterns = [
    path('', views.home , name='home'),
-   path('about', views.about , name='about'),
-   path('project', views.project , name='project'),
-   path('contact', views.contact , name='contact')
+   path('/about', views.about , name='about'),
+   path('/project', views.project , name='project'),
+   path('/contact', views.contact , name='contact')
    
 ]
