2dcdbfea4a69f9e54d0c1eca829919d40f3555fc
vinay4195 <vinay.vk.kumar4@gmail.com>
2022-07-21 23:23:38 +0530

    e commit

--- Home/urls.py
+++ Home/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from Home import views
 urlpatterns = [
-   path('', views.home , name='home'),
+   path('home', views.home , name='home'),
    path('about', views.about , name='about'),
    path('project', views.project , name='project'),
    path('contact', views.contact , name='contact')
